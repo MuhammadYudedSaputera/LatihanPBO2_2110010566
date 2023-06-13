ac7b2a6a98df4dc21476b64610ec8b9ccb4f82d1
MuhammadYudedSaputera <gabinamang@gmail.com>
2023-06-13 12:30:43 +0800

    Menjalankan Program

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -5,10 +5,10 @@ package universitas;
 public class MahasiswaBeraksi {
      public static void main (String[] args) {
         
-        Mahasiswa yuded = new Mahasiswa ;
+        Mahasiswa yuded = new Mahasiswa ("2110010566", " Muhammad Yuded Saputera", " Teknik Informatika");
         
-        System.out.println ("nama mahasiswa: "+ yuded.getnama ());
-        System.out.println ("nama npm: "+ yuded.getnpm ());
-        System.out.println ("nama prodi: "+ yuded.getprodi ());
+        System.out.println ("nama mahasiswa: "+ yuded.getNama ());
+        System.out.println ("nama npm: "+ yuded.getNpm ());
+        System.out.println ("nama prodi: "+ yuded.getProdi ());
     }
 }
